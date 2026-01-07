861761e1739345e4dc908ca2148210d40b805453
Raj kumar singh <162801401+raj8863@users.noreply.github.com>
2026-01-07 22:31:34 +0530

    Update1 api.js

--- frontend/vite-project/src/api.js
+++ frontend/vite-project/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://smart-lead-backend5.onrender.com",
+  baseURL: "https://smart-lead-backend5.onrender.com/api",
 });
 
 export default API;
